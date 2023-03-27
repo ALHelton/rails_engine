c49bf3943cce90fc81c7863a2e760794e0baeff7
Andra Helton <andrahelton@gmail.com>
2023-03-27 17:06:31 -0600

    Add passing test for sends list of merchants

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     module Merchants
-      class SearchController
+      class SearchController < ApplicationController
         
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get "/api/v1/merchants", to: "api/v1/merchants/search#index"
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index]
+    end
+  end
 end
