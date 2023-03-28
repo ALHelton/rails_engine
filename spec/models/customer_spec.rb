e87ce3531fc188255ee80236f443857931b6294f
Andra Helton <andrahelton@gmail.com>
2023-03-28 13:47:52 -0600

    Add updated model creation and associations

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Customer, type: :model do
+  describe "relationships" do
+    it { should have_many :invoices }
+  end
+
+  # describe "validations" do
+  # end
+end
---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Transaction, type: :model do
+  describe "relationships" do
+    it { should belong_to :invoice }
+  end
+
+  # describe "validations" do
+  # end
+end
