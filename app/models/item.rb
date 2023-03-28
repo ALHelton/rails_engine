aaf8315d856c084bbcf56a768b3c1261de0de43d
Andra Helton <andrahelton@gmail.com>
2023-03-28 12:07:24 -0600

    Update Model Validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
 
   validates :name, presence: true
+  validates :description, presence: true
   validates :unit_price, presence: true, numericality: true
+  validates :merchant_id, presence: true
 end
