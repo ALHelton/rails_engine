7808f52ad7a21031b2c37af09ae5990730ae63f7
Andra Helton <andrahelton@gmail.com>
2023-03-28 10:43:55 -0600

    Add test for item show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -25,4 +25,28 @@ describe "Items API", type: :request do
       end
     end
   end
+
+  describe "#show" do
+    before do
+      @item = Item.first
+      get "/api/v1/items/#{@item.id}"
+      @parsed = JSON.parse(response.body, symbolize_names: true)
+    end
+
+    context "when successful" do
+      it "GET one item" do
+        expect(response).to be_successful
+
+        expect(@parsed[:data].keys).to eq([:id, :type, :attributes])
+        expect(@parsed[:data][:id]).to eq(@item.id.to_s)
+        expect(@parsed[:data][:type]).to eq('item')
+        expect(@parsed[:data][:attributes][:name]).to eq(@item.name)
+        expect(@parsed[:data][:attributes][:description]).to eq(@item.description)
+        expect(@parsed[:data][:attributes][:unit_price]).to eq(@item.unit_price)
+        expect(@parsed[:data][:attributes][:merchant_id]).to eq(@item.merchant_id)
+
+        expect(@parsed[:data][:attributes].size).to eq(4)
+      end
+    end
+  end
 end
