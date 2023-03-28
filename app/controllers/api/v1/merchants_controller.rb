e3ae2785742c8af220340250c2f62cd0af4a9cdb
Andra Helton <andrahelton@gmail.com>
2023-03-27 20:09:23 -0600

    Create MerchantsController#index & Serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,9 +1,5 @@
-module Api
-  module V1
-    class MerchantsController < ApplicationController
-      def index
-
-      end
-    end
+class Api::V1::MerchantsController < ApplicationController
+  def index
+    render json: MerchantSerializer.new(Merchant.all)
   end
 end
