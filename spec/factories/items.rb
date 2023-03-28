cbfa61820316dbe337815fcd044cf599b5a7c371
Andra Helton <andrahelton@gmail.com>
2023-03-28 10:29:42 -0600

    Add FactoryBot change

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :item do
     name { Faker::Commerce.product_name }
-    description { Faker::Shakespeare.as_you_like_it_quote }
+    description { Faker::TvShows::TwinPeaks.quote }
     unit_price { Faker::Commerce.price }
     association :merchant
   end
