7782a18e1f7429e9073bf85e09e2cb86444b8ce6
Andra Helton <andrahelton@gmail.com>
2023-03-29 15:17:50 -0600

    Add sad path for ItemsController#create

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,7 +8,13 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    render json: ItemSerializer.new(Item.create(item_params)), status: 201
+    item = Item.create(item_params)
+
+    if item.save
+      render json: ItemSerializer.new(item), status: 201
+    else
+      render status: 422
+    end
   end
 
   def destroy
