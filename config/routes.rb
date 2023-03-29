3a1df9392fb63d19545fff5bd11c52cbfbf31d03
Andra Helton <andrahelton@gmail.com>
2023-03-28 21:05:21 -0600

    Add MerchantItemsController#index and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], controller: "merchant_items"
+      end
       resources :items, only: [:index, :show, :create, :destroy, :update]
     end
   end
