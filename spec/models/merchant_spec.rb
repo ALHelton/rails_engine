a7bc1062acb5b4daaca0271962480ff070157249
Andra Helton <andrahelton@gmail.com>
2023-03-30 16:23:38 -0600

    Add test for Merchant#find_by_name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Merchant, type: :model do
   describe "validations" do
     it { should validate_presence_of :name }
   end
+
+  before do
+    @merch5 = FactoryBot.create(:merchant, name: "Art Shop")
+    
+    @merch1 = FactoryBot.create(:merchant, name: "Artist Shop")
+    @merch2 = FactoryBot.create(:merchant, name: "Practical Arts")
+    @merch3 = FactoryBot.create(:merchant, name: "Jerry's Artarama")
+    @merch4 = FactoryBot.create(:merchant, name: "Walmart")
+    @merch6 = FactoryBot.create(:merchant, name: "Dick Blick")
+  end
+
+  describe "class methods" do
+    it "find_by_name" do
+      expect(Merchant.find_by_name("Art")).to eq(@merch5)
+      expect(Merchant.find_by_name("Art")).to_not eq(@merch1)
+    end
+  end
 end
