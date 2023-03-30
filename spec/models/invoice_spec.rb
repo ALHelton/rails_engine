193b376be27561af02a0e117efbba3fa68bf8be3
Andra Helton <andrahelton@gmail.com>
2023-03-30 07:43:24 -0600

    Add test for item#single_item_invoices

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,7 +8,4 @@ RSpec.describe Invoice, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:items).through(:invoice_items) }
   end
-
-  # describe "validations" do
-  # end
 end
