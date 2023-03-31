3ff9642b0ba56ecf4058c017268c53f4c25e4dd7
Andra Helton <andrahelton@gmail.com>
2023-03-30 21:48:15 -0600

    Add ItemsSearchController#search tests and sad paths

--- spec/requests/api/v1/items_search_request.rb
+++ spec/requests/api/v1/items_search_request.rb
@@ -12,26 +12,136 @@ describe "Items Search API", type: :request do
     @easel = FactoryBot.create(:item, name: "Easel", unit_price: 100.00, merchant_id: @artshop.id)
   end
 
-  context "when successful" do
-    it "gets all case-insensitive items that match a search term, sorted a-z" do
-      get "/api/v1/items/find_all?name=paint"
-      parsed = JSON.parse(response.body, symbolize_names: true)
-      expect(response).to be_successful
-      expect(parsed[:data].size).to eq(3)
-      expect(parsed[:data]).to be_an(Array)
-      expect(parsed[:data][0][:type]).to eq("item")
-      expect(parsed[:data][0][:attributes][:name]).to eq(@acrylic.name)
-      expect(parsed[:data][1][:attributes][:name]).to eq(@oil.name)
-      expect(parsed[:data][2][:attributes][:name]).to eq(@watercolor.name)
+  describe "case-insensitive name search, sorted a-z" do
+    context "when successful" do
+      it "returns all items that match the search" do
+        get "/api/v1/items/find_all?name=paint"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
+        expect(parsed[:data].size).to eq(3)
+        expect(parsed[:data]).to be_an(Array)
+        expect(parsed[:data][0][:type]).to eq("item")
+        expect(parsed[:data][0][:attributes][:name]).to eq(@acrylic.name)
+        expect(parsed[:data][1][:attributes][:name]).to eq(@oil.name)
+        expect(parsed[:data][2][:attributes][:name]).to eq(@watercolor.name)
+      end
     end
 
-    # describe "min_price"
-    it "gets all items that are equal or greater than given price"
+    context "when unsuccessful" do
+      it "returns a 200 error if no match found" do
+        get "/api/v1/items/find_all?name=xxx"
+        parsed = JSON.parse(response.body, symbolize_names: true)
 
-    # describe "max_price"
-    it "gets all items that are equal or less than given price"
+        expect(response).to be_successful
+        expect(parsed).to be_a(Hash)
+        expect(parsed.keys).to eq([:data])
+        expect(parsed[:data]).to eq([])
+      end
 
-    it "gets all items that are within max and min price range"
+      it "can't search by name and price at the same time" do
+        get "/api/v1/items/find_all?name=art&min_price=10.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
 
+        expect(response).to have_http_status(400)
+
+        get "/api/v1/items/find_all?name=art&max_price=20.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(400)
+      end
+    end
+  end
+
+  describe "search all items greater than or equal to min_price" do
+    context "when successful" do
+      it "returns all items that match the search" do
+        get "/api/v1/items/find_all?min_price=20.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:data][0].keys).to eq([:id, :type, :attributes])
+        expect(parsed[:data][0][:id]).to eq(@oil.id.to_s)
+        expect(parsed[:data][0][:type]).to eq('item')
+        expect(parsed[:data][0][:attributes].size).to eq(4)
+        expect(parsed[:data][0][:attributes][:name]).to eq(@oil.name)
+        expect(parsed[:data][1][:attributes][:name]).to eq(@acrylic.name)
+        expect(parsed[:data][2][:attributes][:name]).to eq(@paper.name)
+        expect(parsed[:data][3][:attributes][:name]).to eq(@easel.name)
+      end
+    end
+
+    context "when unsuccessful" do
+      it "returns 400 error when min_price is less than 0" do
+        get "/api/v1/items/find_all?min_price=-10.00"
+        expect(response).to have_http_status(400)
+      end
+    end
+  end
+
+  describe "search all items that are less than or equal to max_price" do
+    context "when successful" do
+      it "returns all items that match the search" do
+        get "/api/v1/items/find_all?max_price=20.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:data][0].keys).to eq([:id, :type, :attributes])
+        expect(parsed[:data][0][:id]).to eq(@oil.id.to_s)
+        expect(parsed[:data][0][:type]).to eq('item')
+        expect(parsed[:data][0][:attributes].size).to eq(4)
+        expect(parsed[:data][0][:attributes][:name]).to eq(@oil.name)
+        expect(parsed[:data][1][:attributes][:name]).to eq(@acrylic.name)
+        expect(parsed[:data][2][:attributes][:name]).to eq(@watercolor.name)
+        expect(parsed[:data][3][:attributes][:name]).to eq(@eraser.name)
+      end
+    end
+
+    context "when unsuccessful" do
+      it "returns 400 error when max_price is less than 0" do
+        get "/api/v1/items/find_all?max_price=-10.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(400)
+        expect(parsed[:errors]).to eq("Price must be greater than 0")
+      end
+    end
+  end
+
+  describe "search all items that are within max and min price range" do
+    context "when successful" do
+      it "returns all items that match the search" do
+        get "/api/v1/items/find_all?min_price=20.00&max_price=100.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to be_successful
+        expect(parsed).to be_a(Hash)
+        expect(parsed[:data][0].keys).to eq([:id, :type, :attributes])
+        expect(parsed[:data][0][:id]).to eq(@oil.id.to_s)
+        expect(parsed[:data][0][:type]).to eq('item')
+        expect(parsed[:data][0][:attributes].size).to eq(4)
+        expect(parsed[:data][0][:attributes][:name]).to eq(@oil.name)
+        expect(parsed[:data][1][:attributes][:name]).to eq(@acrylic.name)
+        expect(parsed[:data][2][:attributes][:name]).to eq(@paper.name)
+        expect(parsed[:data][3][:attributes][:name]).to eq(@easel.name)
+      end
+    end
+
+    context "when unsuccessful" do
+      it "returns 400 error when max_price or min_price is less than 0" do
+        get "/api/v1/items/find_all?min_price=-10.00&max_price=20.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(400)
+        expect(parsed[:errors]).to eq("Price must be greater than 0")
+
+        get "/api/v1/items/find_all?min_price=-30.00&max_price=-20.00"
+        parsed = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response).to have_http_status(400)
+        expect(parsed[:errors]).to eq("Price must be greater than 0")
+      end
+    end
   end
 end
