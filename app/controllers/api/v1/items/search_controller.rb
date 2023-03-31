94577c15201ec551b54d0ebd882e4c5aa72bfab9
Andra Helton <andrahelton@gmail.com>
2023-03-30 21:49:27 -0600

    Add ItemsSearchController#search

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,15 +1,39 @@
 class Api::V1::Items::SearchController < ApplicationController
   def search
-    items_by_name = Item.find_by_name(params[:name])
-    if items_by_name == nil
-      render json: { data: [] }
-    else
+    if params[:name] && params[:min_price] && params[:max_price]
+      render json: { errors: "Can't search by name and price" }, status: 400
+ 
+    elsif params[:name] && !params[:min_price] && !params[:max_price]
+      items_by_name = Item.find_by_name(params[:name])
       render json: ItemSerializer.new(items_by_name)
-    end
+ 
+    elsif params[:min_price] && params[:max_price] && !params[:name]
+      if params[:min_price].to_i >= 0 && params[:max_price].to_i >= 0
+        items_between_prices = Item.find_between_prices(params[:min_price], params[:max_price])
+        render json: ItemSerializer.new(items_between_prices)
+      else
+        render json: { errors: "Price must be greater than 0" }, status: 400
+      end
+ 
+    elsif params[:min_price] && !params[:name] && !params[:max_price]
+      if params[:min_price].to_i >= 0 
+        items_over_min_price = Item.find_at_or_above_price(params[:min_price])
+        render json: ItemSerializer.new(items_over_min_price)
+      else
+        render json: { errors: "Price must be greater than 0" }, status: 400
+      end
+     
+    elsif params[:max_price] && !params[:name] && !params[:min_price]
+      if params[:max_price].to_i >= 0
+        items_under_max_price = Item.find_at_or_below_price(params[:max_price])
+        render json: ItemSerializer.new(items_under_max_price)
+      else
+        render json: { errors: "Price must be greater than 0" }, status: 400
+      end
 
-    # items_by_price = Item.find_by_price(params[:unit_price])
-    # items_over_min_price = Item.find_above_price(params[:unit_price])
-    # items_over_max_price = Item.find_below_price(params[:unit_price])
-    # items_between_price = Item.find_between_price(params[:unit_price])
+    else
+      render json: { errors: "Missing Parameters" }, status: 400
+    end
   end
+
 end
